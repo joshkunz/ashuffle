8f7e3d8047d9775f580a2c9e5ed4fc8eed40ee43
Josh Kunz <josh@kunz.xyz>
2019-03-21 20:40:47 -0700

    Adds shuffle tests

--- src/t/shuffle_test.c
+++ src/t/shuffle_test.c
@@ -1,3 +1,6 @@
+#define _GNU_SOURCE
+#include <errno.h>
+#include <stdarg.h>
 #include <stdbool.h>
 #include <string.h>
 
@@ -20,10 +23,119 @@ void test_basic() {
     shuffle_free(&chain);
 }
 
+// sprintf, but we crash if we can't allocate a buffer big enough to store
+// the string we build. Caller must free the returned string.
+char * xsprintf(const char * fmt, ...) {
+    va_list l;
+    va_start(l, fmt);
+
+    char * out;
+    if (vasprintf(&out, fmt, l) == -1) {
+        perror("failed to asprintf");
+        exit(1);
+    }
+
+    va_end(l);
+
+    return out;
+}
+
+// Returns true if the list `lst` contains the string `a`.
+bool check_contains(const char * a, const char * lst[], size_t lst_len) {
+    for (size_t i = 0; i < lst_len; i++) {
+        if (strcmp(a, lst[i]) == 0) {
+            return true;
+        }
+    }
+    return false;
+}
+
+// Returns true if all items in `lst` are unique.
+bool check_unique(const char * lst[], size_t lst_len) {
+    for (size_t i = 0; i < lst_len; i++) {
+        for (size_t j = i+1; j < lst_len; j++) {
+            if (strcmp(lst[i], lst[j]) == 0) {
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
+void test_multi() {
+    unsigned test_rounds = 5000;
+
+    struct shuffle_chain chain;
+    shuffle_init(&chain, 1);
+
+    const char * test_items[] = {"item 1", "item 2", "item 3"};
+
+    shuffle_add(&chain, test_items[0]);
+    shuffle_add(&chain, test_items[1]);
+    shuffle_add(&chain, test_items[2]);
+
+    for (unsigned i = 0; i < 5000; i++) {
+        const char * item = shuffle_pick(&chain);
+        if (!check_contains(item, test_items, 3)) {
+            fail("pick %u rounds", test_rounds);
+            diag("  fail on round: %u", i);
+            diag("  input: \"%s\"", item);
+            goto fail;
+        }
+    }
+    pass("pick %u rounds", test_rounds);
+fail:
+    shuffle_free(&chain);
+}
+
+void test_window_of_size(const unsigned window_size) {
+    struct shuffle_chain chain;
+    shuffle_init(&chain, window_size);
+
+    for (unsigned i = 0; i < window_size; i++) {
+        char * item = xsprintf("item %u", i);
+        shuffle_add(&chain, item);
+        free(item);
+    }
+
+    // Our buffer of picked items needs to be one larger than the window size
+    // so we can test that there is at least one repeat.
+    const char * picked[window_size + 1];
+
+    for (unsigned i = 0; i < window_size + 1; i++) {
+        picked[i] = shuffle_pick(&chain);
+    }
+
+    // Just looking at the first `window_size` items, they should all be
+    // unique, since they came out of the window.
+    ok(check_unique(picked, window_size),
+       "pick window_size (%u) items, all are unique", window_size);
+
+    // If we check all items there should be at least one repeat. Since
+    // we just checked that the first three items are unique, then we know
+    // that the last item is the repeat, as we expect.
+    ok(!check_unique(picked, window_size + 1),
+       "pick window_size (%u) + 1 items, there is one repeat", window_size);
+
+    shuffle_free(&chain);
+}
+
+void test_windowing() {
+    // test all the small windows.
+    for (unsigned i = 1; i <= 25; i++) {
+        test_window_of_size(i);
+    }
+    // Test a couple big, round windows.
+    test_window_of_size(50);
+    test_window_of_size(100);
+}
+
 int main() {
     plan(NO_PLAN);
 
     test_basic();
+    test_multi();
+    test_windowing();
 
     done_testing();
 }
