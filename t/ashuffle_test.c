3bcc8ef74d8fbe9950f9e6c715571faee5cfb787
Josh Kunz <josh@kunz.xyz>
2019-08-31 12:36:25 -0700

    Adds build_songs_file (check) test
    This test is largely similar to the `build_songs_file` nocheck case, but
    also checks a rule.

--- t/ashuffle_test.c
+++ t/ashuffle_test.c
@@ -19,9 +19,6 @@
 
 // Main tests
 // connect
-// build_songs_file
-//  + with/without nocheck
-// (DONE) build_songs_mpd
 // shuffle_single
 // shuffle_idle
 //  + try_first
@@ -31,6 +28,14 @@ void list_push_song(struct list *l, struct mpd_song *s) {
     struct datum d = {
         .length = sizeof(struct mpd_song),
         .data = (void *)s,
+    };
+    list_push(l, &d);
+}
+
+void list_push_empty(struct list *l) {
+    struct datum d = {
+        .length = 0,
+        .data = NULL,
     };
     list_push(l, &d);
 }
@@ -95,6 +100,7 @@ void test_build_songs_mpd_filter() {
     mpd_connection_free(&c);
     rule_free(&artist_match);
     shuffle_free(&chain);
+    list_free(&ruleset);
 }
 
 void xfwrite(FILE *f, const char *msg) {
@@ -187,12 +193,112 @@ void test_build_songs_file_nocheck() {
     shuffle_free(&chain);
 }
 
+void test_build_songs_file_check() {
+    // step 1. Initialize the MPD connection.
+    struct mpd_connection c;
+    memset(&c, 0, sizeof(c));
+
+    // step 2. Build the ruleset, and add an exclusions for __not_artist__
+    struct list ruleset;
+    list_init(&ruleset);
+
+    struct song_rule artist_match;
+    rule_init(&artist_match);
+    set_tag_name_iparse_result("artist", MPD_TAG_ARTIST);
+    // Exclude all songs with the artist "__not_artist__".
+    rule_add_criteria(&artist_match, "artist", "__not_artist__");
+    struct datum artist_match_d = {
+        .data = (void *)&artist_match,
+        .length = sizeof(struct song_rule),
+    };
+    list_push(&ruleset, &artist_match_d);
+
+    // step 3. Prepare the shuffle_chain.
+    struct shuffle_chain chain;
+    const unsigned window_size = 2;
+    shuffle_init(&chain, window_size);
+
+    // step 4. Prepare our songs/song list. The song_list will be used for
+    // subsequent calls to `mpd_recv_song`.
+    TEST_SONG(song_a, TAG(MPD_TAG_ARTIST, "__artist__"));
+    TEST_SONG(song_b, TAG(MPD_TAG_ARTIST, "__not_artist__"));
+    TEST_SONG(song_c, TAG(MPD_TAG_ARTIST, "__artist__"));
+
+    // When matching each song URI against the rules, ashuffle executes
+    // mpd_recv_song twice. Once to receive the actual song object from
+    // libmpdclient, and once to "finish" the iteration. So, for each URI we
+    // want to test, we also need to insert an empty entry after it.
+    list_init(&c.song_iter);
+    list_push_song(&c.song_iter, &song_a);
+    list_push_empty(&c.song_iter);
+    list_push_song(&c.song_iter, &song_b);
+    list_push_empty(&c.song_iter);
+    list_push_song(&c.song_iter, &song_c);
+    list_push_empty(&c.song_iter);
+
+    // step 5. Set up our test input file, but writing the URIs of our songs.
+    FILE *f = tmpfile();
+    if (f == NULL) {
+        perror("couldn't open tmpfile");
+        abort();
+    }
+
+    xfwriteln(f, song_a.uri);
+    xfwriteln(f, song_b.uri);
+    xfwriteln(f, song_c.uri);
+
+    // rewind, so build_songs_file can see the URIs we've written.
+    rewind(f);
+
+    // step 6. Run! (and validate)
+
+    int result = build_songs_file(&c, &ruleset, f, &chain, true);
+    cmp_ok(result, "==", 0, "build_songs_file check returns ok");
+    cmp_ok(shuffle_length(&chain), "==", 2,
+           "build_songs_file_check: 2 songs added to shuffle chain");
+
+    // This check works like the nocheck case, but instead of expecting us
+    // to pick all 3 songs that were written into the input file, we only want
+    // to pick song_a and song_c which are not excluded by the ruleset
+    const char *want[] = {song_a.uri, song_c.uri};
+    const char *got[2];
+    got[0] = shuffle_pick(&chain);
+    got[1] = shuffle_pick(&chain);
+
+    qsort(want, STATIC_ARRAY_LEN(want), sizeof(want[0]), compare_str);
+    qsort(got, STATIC_ARRAY_LEN(got), sizeof(got[0]), compare_str);
+
+    assert(STATIC_ARRAY_LEN(want) == window_size &&
+           "number of wanted URIs should match the window size");
+    static_assert(STATIC_ARRAY_LEN(want) == STATIC_ARRAY_LEN(got),
+                  "length of want and got should be equal so all elements can "
+                  "be compared");
+
+    for (unsigned i = 0; i < STATIC_ARRAY_LEN(want); i++) {
+        bool equal = strcmp(want[i], got[i]) == 0;
+        ok(equal, "build_songs_file_check: want == got at %u", i);
+        if (!equal) {
+            diag("(want) %s != (got) %s", want[i], got[i]);
+        }
+    }
+
+    // cleanup.
+
+    fclose(f);
+
+    mpd_connection_free(&c);
+    shuffle_free(&chain);
+    rule_free(&artist_match);
+    list_free(&ruleset);
+}
+
 int main() {
     plan(NO_PLAN);
 
     test_build_songs_mpd_basic();
     test_build_songs_mpd_filter();
     test_build_songs_file_nocheck();
+    test_build_songs_file_check();
 
     done_testing();
 }
