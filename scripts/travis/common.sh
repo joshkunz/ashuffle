9409ddeffae3f8198a35c830f635a74601260da6
Josh Kunz <josh@kunz.xyz>
2020-07-19 21:53:07 -0700

    travis: Always install a recent version of Go
    We use some language (like syntax) features from fairly recent versions
    of Go in the integration tests. If we run even gofmt with an old
    version, we can fail to recognize the source, and fail lint checks.

--- scripts/travis/common.sh
+++ scripts/travis/common.sh
@@ -1,16 +1,20 @@
 MESON_VERSION="0.54.0"
+GO_VERSION="1.14.6"
+GIMMIE_URL="https://raw.githubusercontent.com/travis-ci/gimme/master/gimme"
 
 die() {
     echo "$@" >&2
     exit 1
 }
 
+install_go() {
+    goversion="$1"
+    # Gimmie outputs envrionment variables, so we need to eval them here.
+    eval "$(curl -sL "${GIMMIE_URL}" | GIMME_GO_VERSION="${goversion}" bash)"
+}
+
 build_meta() {
-    prevwd="${PWD}"
-    godir="$(mktemp -d)"
-    wget --quiet -O- https://dl.google.com/go/go1.14.2.linux-amd64.tar.gz | tar -C "${godir}" -xz --strip-components=1
-    "${godir}/bin/go" version
-    (cd tools/meta && GOROOT="${godir}" GO11MODULE=on "${godir}/bin/go" build)
+    (cd tools/meta && GO11MODULE=on go build)
 }
 
 setup() {
@@ -21,6 +25,7 @@ setup() {
         sudo env DEBIAN_FRONTEND=noninteractive apt-get install -y \
             clang-9 \
             clang-tidy-9 \
+            clang-format-9 \
             cmake \
             libmpdclient-dev \
             ninja-build \
@@ -28,5 +33,6 @@ setup() {
             python3 python3-pip python3-setuptools python3-wheel \
     || die "couldn't apt-get required packages" 
     sudo pip3 install meson=="${MESON_VERSION}" || die "couldn't install meson"
+    install_go "${GO_VERSION}"
     build_meta
 }
