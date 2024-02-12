db89a8fa642909706e258c3680e9772da983a5d7
Josh Kunz <josh@kunz.xyz>
2024-02-12 00:05:03 -0800

    Try to stop invalidating caches when building meta

--- scripts/github/common.sh
+++ scripts/github/common.sh
@@ -1,9 +1,6 @@
 # renovate: datasource=pypi depName=meson
 MESON_VERSION="1.3.1"
-# renovate: datasource=github-tags depName=golang/go
-GO_VERSION="go1.20.1"
 LLVM_RELEASE="14"
-GIMMIE_URL="https://raw.githubusercontent.com/travis-ci/gimme/master/gimme"
 
 CLANG_CC="clang-${LLVM_RELEASE}"
 CLANG_CXX="clang++-${LLVM_RELEASE}"
@@ -16,19 +13,8 @@ die() {
     exit 1
 }
 
-install_go() {
-    local want_ver="${GO_VERSION}"
-    if test $# -gt 0; then
-        want_ver="$1"
-    fi
-    # Sed out the 'go...' prefix.
-    goversion="$(echo "${want_ver}" | sed -E 's/^go(.*)/\1/')"
-    # Gimmie outputs envrionment variables, so we need to eval them here.
-    eval "$(curl -sL "${GIMMIE_URL}" | GIMME_GO_VERSION="${goversion}" bash)"
-}
-
 build_meta() {
-    (cd tools/meta && GO11MODULE=on go build)
+    (cd tools/meta && GO11MODULE=on go build -o "${RUNNER_TEMP}/meta")
 }
 
 setup() {
@@ -48,6 +34,4 @@ setup() {
             python3 python3-pip python3-setuptools python3-wheel \
     || die "couldn't apt-get required packages"
     sudo pip3 install meson=="${MESON_VERSION}" || die "couldn't install meson"
-    install_go "${GO_VERSION}"
-    build_meta
 }
