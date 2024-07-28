c4cb8a766b6a83b392e4c1c3df16a2207e9378b2
Josh Kunz <josh@kunz.xyz>
2024-07-28 10:36:45 -0700

    Fixes up the build for newer compiler/libc++ releases

--- tools/meta/commands/mpd/mpd.go
+++ tools/meta/commands/mpd/mpd.go
@@ -5,7 +5,7 @@ import (
 	"log"
 	"os"
 	"path/filepath"
-	"sort"
+	"slices"
 	"strings"
 
 	"github.com/urfave/cli/v2"
@@ -23,9 +23,10 @@ func applyPatches(dir string) error {
 		return err
 	}
 
-	sort.Strings(patches)
+	slices.Sort(patches)
 
 	for _, patch := range patches {
+		log.Printf("Applying patch %q", patch)
 		f, err := os.Open(patch)
 		if err != nil {
 			return fmt.Errorf("failed to open patch %q: %w", patch, err)
@@ -41,17 +42,16 @@ func applyPatches(dir string) error {
 }
 
 func install(ctx *cli.Context) error {
-	// Not being able to fined a patch root is only a problem for older
-	// releases, and we'll only do those once in a while. The majority of
-	// the time, we don't care if it exists. Save the error here, and
-	// then print it later only if we actually need the patch root.
 	// Note: We do this first, so we can get the path before workspace.New()
 	// moves us.
 	var patchRoot string
-	var patchRootErr error
-	patchRoot, patchRootErr = filepath.Abs(ctx.String("patch_root"))
-	if patchRootErr != nil {
-		patchRoot = ""
+	patchRoot, err := filepath.Abs(ctx.String("patch_root"))
+	if err != nil {
+		return fmt.Errorf("failed to find patch root %q: %w", ctx.String("patch_root"), err)
+	}
+	// Make sure we actually have a patch root.
+	if patchRoot == "" {
+		return fmt.Errorf("patch root is empty")
 	}
 
 	ws, err := workspace.New()
@@ -79,36 +79,29 @@ func install(ctx *cli.Context) error {
 		return err
 	}
 
-	if v.Minor == 19 && v.Patch < 15 {
-		if patchRoot == "" {
-			return fmt.Errorf("failed to find patch root %q: %w", ctx.String("patch_root"), err)
+	versionPatchDir := filepath.Join(patchRoot, "mpd", v.String())
+	if stat, err := os.Stat(versionPatchDir); err == nil {
+		// If the patch dir exists, then try to apply all the patches
+		if !stat.IsDir() {
+			return fmt.Errorf("patch dir path %q is not a directory", versionPatchDir)
 		}
-		if err := applyPatches(filepath.Join(patchRoot, "0.19", "pre-15")); err != nil {
-			return err
+		if err := applyPatches(versionPatchDir); err != nil {
+			return fmt.Errorf("failed to apply patches: %w", err)
 		}
+	} else if os.IsNotExist(err) {
+		// If it doesn't exist, then just log that we're not applying anything
+		log.Printf("Patch dir %q not found, no patches to apply", versionPatchDir)
+	} else {
+		return fmt.Errorf("failed to locate patch dir: %w", err)
 	}
 
-	var proj project.Project
-	if v.Minor < 21 {
-		p, err := project.NewAutomake(ws.Root, project.AutomakeOptions{
-			// Need to use an old compiler here as well.
-			CCompiler:   "gcc-9",
-			CXXCompiler: "g++-9",
-		})
-		if err != nil {
-			return err
-		}
-		proj = p
-	} else {
-		p, err := project.NewMeson(ws.Root, project.MesonOptions{
-			BuildType:      project.BuildDebugOptimized,
-			BuildDirectory: "build/release",
-			Extra:          []string{"-Db_ndebug=true"},
-		})
-		if err != nil {
-			return err
-		}
-		proj = p
+	proj, err := project.NewMeson(ws.Root, project.MesonOptions{
+		BuildType:      project.BuildDebugOptimized,
+		BuildDirectory: "build/release",
+		Extra:          []string{"-Db_ndebug=true"},
+	})
+	if err != nil {
+		return err
 	}
 	return project.Install(proj, ctx.String("prefix"))
 }
